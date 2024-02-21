3145f991cd9a93ec662e96d16ec13f71904c46ed
Logan Cox <mail@logan-cox.com>
2024-02-21 14:12:33 +0000

    feat: extract URLs from HTML

--- crawl.js
+++ crawl.js
@@ -1,3 +1,4 @@
+const { JSDOM } = require('jsdom')
 
 function normalizeURL(url) {
 	urlObj = new URL(url)
@@ -9,8 +10,23 @@ function normalizeURL(url) {
 	return `${urlObj.host}${urlObj.pathname}` 
 }
 
+function extractURLsFromHTML(htmlBody, baseURL) {
+	let returnList = []
+	let dom = new JSDOM(htmlBody)
+	let links = dom.window.document.querySelectorAll('a')
+
+	for (link of links) {
+		if (link.href[0] === '/') {
+			link.href = new URL(`${baseURL}${link.href}`)
+		}
+		returnList.push(new URL(link.href).href)
+	}
+
+	return returnList	
+}
+
 module.exports = {
 	normalizeURL,
-	
+	extractURLsFromHTML,	
 }
 
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, extractURLsFromHTML } = require('./crawl.js')
 
 test('Trailing / is removed from HTTPS URL', () => {
   expect(normalizeURL('https://google.com/path/')).toBe('google.com/path');
@@ -12,3 +12,80 @@ test('Trailing / is removed from HTTP URL', () => {
 test('No changes on URL format we expect', () => {
   expect(normalizeURL('https://google.com/path')).toBe('google.com/path');
 });
+
+
+test('Extract single URL from HTML', () => {
+  expect(extractURLsFromHTML(
+`
+<html>
+  <body>
+    <a href="https://google.com">google</a>
+  </body>
+</html>
+`, 
+      'https://test.com'
+    )
+  ).toStrictEqual(['https://google.com/']);
+});
+
+test('Extract multiple URLs from HTML', () => {
+  expect(
+    extractURLsFromHTML(
+`
+<html>
+  <body>
+    <a href="https://google.com">google</a>
+    <a href="https://youtube.com">youtube</a>
+  </body>
+</html>
+`, 
+      'https://test.com'
+    )
+  ).toStrictEqual(['https://google.com/', 'https://youtube.com/']);
+});
+
+test('No URL in HTML', () => {
+  expect(
+    extractURLsFromHTML(
+`
+<html>
+  <body>
+  </body>
+</html>
+`, 
+      'https://test.com'
+    )
+  ).toStrictEqual([]);
+});
+
+test('Parse relative links', () => {
+  expect(
+    extractURLsFromHTML(
+`
+<html>
+  <body>
+    <a href="/page">other local page</a>
+  </body>
+</html>
+`,
+      'https://test.com'
+    )
+  ).toStrictEqual(['https://test.com/page']);
+});
+
+test('Parse relative and absoloute links', () => {
+  expect(
+    extractURLsFromHTML(
+`
+<html>
+  <body>
+    <a href="/page">other local page</a>
+    <a href="https://google.com/page">google</a>
+  </body>
+</html>
+`,
+      'https://test.com'
+    )
+  ).toStrictEqual(['https://test.com/page', 'https://google.com/page']);
+});
+
